984d933473cebf97a7000316544b1936a239adaf
Sal <gramophoneturtle@gmail.com>
2024-04-11 21:44:28 -0700

    Cleaned up artwork page - removed tabbed JS and for slideshow, hid the prev/next buttons when at the beginning/end to prevent looping around

--- src/js/TabbedImageGallery.js
+++
@@ -1,8 +0,0 @@
-/** https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp */
-
-function expandArtWork(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-    expandImg.src = imgs.src;
-    expandImg.alt = imgs.alt;
-    expandImg.parentElement.style.display = "block";
-}
