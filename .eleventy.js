e57805559daa7f6d887045e1607da9ce9425b83a
Sal <gramophoneturtle@gmail.com>
2024-04-14 21:34:56 -0700

    Now actually generating images kinda from source png file - but it's in the wrong directory and is ignoring previous path

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,6 @@
 const Image = require("@11ty/eleventy-img");
 const path = require('path')
 
-// (async () => {
-// 	let url = "";
-// 	let stats = await Image(url, {
-// 		widths: [300, 600, auto],
-//     // Array of file format extensions or "auto"
-// 		formats: ["webp", "jpeg"],
-
-//     outputDir: "./public/img/"
-
-// 	});
-
-// 	console.log(stats);
-// })();
-
-// ex for custom filename - goes here?
-// const path = require("path");
-// const Image = require("@11ty/eleventy-img");
-
-// await Image("./test/bio-2017.jpg", {
-// 	widths: [300],
-// 	formats: ["auto"],
-// 	filenameFormat: function (id, src, width, format, options) {
-// 		const extension = path.extname(src);
-// 		const name = path.basename(src, extension);
-
-// 		return `${name}-${width}w.${format}`;
-// 	},
-// });
-
 module.exports = function (eleventyConfig) {
   // --- PASS HROUGHS
   eleventyConfig.addPassthroughCopy("./src/css");
@@ -38,17 +9,19 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
 
-  // --- START, eleventy-img
+  // -- START, eleventy-img
 	function imageShortcode(src, alt, sizes="(min-width: 1024px) 100vw, 50vw") {
 		console.log(`Generating image(s) from:  ${src}`)
 		let options = {
-			widths: [200, 800, 1500],
-			formats: ["webp", "jpeg"],
-			urlPath: "./src/img/",
-			outputDir: "./public/img/",
+			widths: [600, 900, 1500],
+			formats: ["webp", "jpeg","png"],
+			urlPath: "/img/fin/",
+			outputDir: "./public/img/fin/",
+
 			filenameFormat: function (id, src, width, format, options) {
-				const extension = path.extname(src)
+        const extension = path.extname(src)
 				const name = path.basename(src, extension)
+        console.log(`Generating image(s) from name:  ${name}`)
 				return `${name}-${width}w.${format}`
 			}
 		}
