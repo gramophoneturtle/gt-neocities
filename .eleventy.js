6ed6b63ee4f23f49c5c2ea08b38b6a7dbc0fc2f9
gramophoneturtle <163605491+gramophoneturtle@users.noreply.github.com>
2024-10-04 21:51:44 -0600

    24.10 v1 - Related Series (#46)
    SUMMARY
    Main
     -   Related Series/AU section added for TWEWY
     -   Video support tested for gifs
     -   Some light mode CSS clean up
    
    Conversion script
     -   Cleaned up output
     -   Added one video support for testing
     -   Added creating the unique URL here so it can be referenced for related series/AU
    
    Artwork:
     -   Why Oh Why...
     -   Hanyakoma Hoodie!
     -   Possession
     -   Home
     -   Yoshi-ya Hoodie!
    
    COMMITS
    
    * Start of extracting Related Series / Sub Series / AUs
    
    * Made a separate related series JSON. Will build a separate pages for these I think.
    Also determined the safe url for each page in the python file in order to create refs/links
    
    * Set up sample JSON structure to display as how I'd want it to for related listings
    
    * moved test pages to a secrets folder
    
    * Created a home "Related" page for TWEWY related series and AUs
    
    * Update functionality added to the related series
    
    * Cleaned up related page header title stuff
    
    * Reading indices from excel file and attempt to link back to the related page
    
    * Artworks link back to the related series page
    
    * Added next update section for related linking and added links to all twewy related series from the Art page and from any involved Artwork page
    
    * Fixed showing the series section on artwork when there is only one related series they are a part of
    
    * Added "gif" as video support. 1 video test at the moment with adding Yoshi-ya Hoodie
    
    * Add Yoshia-ya Hoodie to Updates
    
    * Removed extra slash in building the URL keys
    
    * Related Series: Sorting by index
    
    * Renamed the Kubokupo series to Hoodie Buddies
    
    Artwork:
    - Hanyakoma Hoodie!
    - Possession
    
    * merged healthy does of chaos caps diff issue
    
    * Artwork: Why Oh Why
    
    * Combined Composer Neku AU labels for now - since PW builds off of the general ones. Added Update URLs for the new artworks
    
    * All artworks will have the thumbnail info populated as blank strings by default
    
    * CSS -  Showing artwork border made thicker b/c in light mode it appears too thin
    
    * CSS
    - Related Series Entries: made it! Image on left, name/title on right/remaining space. Removed showing the index
    - general light mode colour fixing
    - displaying artwork: added bg colour to the page X/Y and added a nice border around it and the slideshow thumbnails so it looks nicer. Tested in light and dark mode
    
    * styled the main list of related series to appear cleaner
    
    * RSS feed attempt 2 - now using meta data at least
    
    * Art:
    - Home added
    
    Update
    - Updated urls for: Fashion and Hazukweh
    
    * RSS Feeds updaterd

--- .eleventy.js
+++ .eleventy.js
@@ -12,13 +12,22 @@ const Image = require("@11ty/eleventy-img");
 // 	console.log(stats);
 // })();
 
+
+
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+
+
 module.exports = function (eleventyConfig) {
+  // PASSTHROUGH COPIES ------------------------------------------------------------------- //
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/vid");
   eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addPassthroughCopy("./src/rss");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
 
+	eleventyConfig.addPlugin(pluginRss);
 
 
 //   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
@@ -45,12 +54,16 @@ module.exports = function (eleventyConfig) {
 // 	});
 
   // when tags are spoilers and NO SPOILERS, gonna end up with nothing!
-  // However, can use this for spliting TWEWY OG, NEO and ALL
+  // However, can use this for having:
+  // - split TWEWY OG, NEO 
+  // - also have them ALL combined!
   eleventyConfig.addCollection("allTWEWY", function (collectionApi) {
     // ge filtered by Tags - is requiring BOTH tags - so good for spoiler tagging? 
     return collectionApi.getFilteredByTags("twewyArt2All", "twewyArtNoSpoilers");
   });
 
+  // SORTING ------------------------------------------------------------------- // 
+  // Sort by TITLE
   eleventyConfig.addCollection("twewySeriesArtAllByTitle", (collection) =>
     collection.getFilteredByTags("twewyArt2").sort((a, b) => {
       let nameA = a.data.twewyart.title.toUpperCase();
@@ -61,6 +74,7 @@ module.exports = function (eleventyConfig) {
     })
   );
 
+  // Sort by DATE
   eleventyConfig.addCollection("twewySeriesArtAllByDate", function (collectionApi) {
     // ge filtered by Tags - is requiring BOTH tags - so good for spoiler tagging? 
     return collectionApi.getFilteredByTags("twewyArt2").sort(function (a, b) {
@@ -74,6 +88,7 @@ module.exports = function (eleventyConfig) {
     });
   });
 
+  // Sort by DATE
   eleventyConfig.addCollection("twewyOGArtAllByDate", function (collectionApi) {
     // ge filtered by Tags - is requiring BOTH tags - so good for spoiler tagging? 
     return collectionApi.getFilteredByTags("twewyOGArt").sort(function (a, b) {
@@ -87,7 +102,7 @@ module.exports = function (eleventyConfig) {
     });
   });
 
-
+  // RETURN ------------------------------------------------------------------- //
   return {
     dir: {
       input: "src",
--- data_conversion/from_excel_to_json.py
+++ data_conversion/from_excel_to_json.py
@@ -2,11 +2,15 @@
 # Winwdows
 # > py -m pip install pandas
 import pandas
-import logging, sys
+import urllib3
 import json
 import os
+import re
 
+# Excel Database Filename
 filename_input = "Where Is My Art.xlsx"
+
+# GENERATE - TWEWY JSON Files
 filename_output_twewy_og_all = "twewy-art.json"
 filename_output_twewy_og_nospoilers = "twewy-art-spoiler-free.json"
 filename_output_twewy_neo_all = "twewy-neo-art.json"
@@ -14,14 +18,24 @@ filename_output_twewy_neo_nospoilers = "twewy-neo-art-spoiler-free.json"
 filename_output_twewy_series_all = "twewy-series.json"
 filename_output_twewy_series_nospoilers = "twewy-series-spoiler-free.json"
 
+# GENERATE - PERSONA 5 JSON Files
 filename_output_p5 = "art-persona5.json"
+
+# GENERATE - Related Series Files
+filename_output_related_twewy = "relatedtwewy.json"
+
 file_path_input = os.path.join(os.getcwd(),"data_conversion\\", filename_input)
 file_path_output_path = os.path.join(os.getcwd(),"src\\_data\\")
 
 date_column_name = "Earliest Date"
 max_num_images = 8
+max_num_videos = 1
+
+related_series_list = []
 
 # functions
+
+# IMAGE LISTS -------------------------------------
 def make_img_list(_row: pandas.Series) -> list:
     '''
     Must have IMG source URL AND Alt Text
@@ -78,13 +92,106 @@ def get_img_url(rw, st_nm) -> str:
 
     return return_url
 
-def make_url_dict(mydataframe: pandas.DataFrame) -> list:
+def make_vid_list(_row: pandas.Series) -> list:
+    vid_list = [] 
+
+    for i in range(1, max_num_videos+1):
+        # init
+        key_vid = 'VID {0}'.format(i)
+        artwork_id = _row['Artwork'].replace("\n","")[0:40]
+        
+        # guards
+        # if there is no more videos, we don't need to continue
+        if _row[key_vid] == "" :
+            break
+
+        test_url = get_img_url(_row, key_vid)
+        # make sure we don't have the src directory - might get included by accident!
+        if "src" in test_url:
+            print("!! VIDEO URL contains src. Fixing for |{0}| #{1}".format(artwork_id, i))
+            test_url = test_url.replace("src","")
+
+        # we have an image and alt text - good to add
+        vid_list.append({'id': i, 'url': test_url})
+
+    return vid_list
+
+# RELATED DICTIONARY -------------------------------------
+def find_rel_dict_ser_name_index(lst, key, value):
+    for i, dic in enumerate(lst):
+        if dic[key] == value:
+            return i
+    return -1
+
+def update_related_dictionary(rw, aw_dict):
+
+    artworkname = rw['Artwork'].replace("\n","")[0:40]
+
+    foundseries = rw['RelatedSeries'].split(";")
+    foundindices = rw['RelatedSeriesOrder'].split(";")
+
+    for i_index, i_name in enumerate(foundseries):
+        i_name = i_name.strip()
+
+        index_dict = find_rel_dict_ser_name_index(related_series_list, "SeriesName", i_name)
+
+        # Get the index of the entry in the series
+        int_i_index = -1
+        if i_index < len(foundindices):
+            try:
+                int_i_index = foundindices[i_index]
+                int_i_index = int(int_i_index)
+            except ValueError:
+                int_i_index = -1
+                print("    Related for {0} -AU/Series- {1}".format(artworkname,i_name))
+                print("      !! TODO: Current index [{0}] for foundindices is not an int! Default to [-1].".format(i_index,foundindices[i_index]))
+        else:
+            print("    Related for {0} -AU/Series- {1}".format(artworkname,i_name))
+            print("      '! Current index [{0}] is less than indices found: {1}. Default to [-1].".format(i_index,foundindices))
+        
+        new_entry = {
+                'Index': int_i_index,
+                'Name': aw_dict['title'],
+                'URL': aw_dict['UniqueURLKey'],
+                'ThumbnailURL': aw_dict['thumbnailUrl'],
+                'ThumbnailAlt': aw_dict['thumbnailAlt']
+            }
+
+        # Add new seriesand first entry
+        if index_dict == -1:
+            rel_dictionary = {
+                'SeriesName': i_name,
+                "SeriesURL": urlify(i_name),
+                'SeriesEntries': [ new_entry ]
+            }
+            # Add with defaults
+            related_series_list.append(rel_dictionary)
+        # Add new entry to existing series
+        else:
+            related_series_list[index_dict]["SeriesEntries"].append(new_entry)
+    return 
+
+# https://stackoverflow.com/questions/1007481/how-to-replace-whitespaces-with-underscore
+def urlify(s):
+
+    # Remove all non-word characters (everything except numbers and letters)
+    s = re.sub(r'[^\w\s-]', '', s).strip() 
+
+    # Replace all runs of whitespace with a single dash
+    s = re.sub(r"\s+", '-', s)
+
+    s = urllib3.util.parse_url(s).url
+
+    return s
+
+def make_url_dict(mydataframe: pandas.DataFrame,base_url) -> list:
     return_list = []
     dict_key = "ARTWORK"
     urls_key = "urls"
     tmp_dict = {dict_key: []}
     for _, row in mydataframe.iterrows():
 
+        # MAIN INFO
         artwork_dictionary =  {
             'title': row['title'], 
             'summary': row['summary'], 
@@ -95,17 +202,27 @@ def make_url_dict(mydataframe: pandas.DataFrame) -> list:
         
         artwork_dictionary['date'] = row[date_column_name]
         artwork_dictionary['dateYear'] = row[date_column_name][0:4]
-        artwork_dictionary['uniqueUrl'] = "{0}-{1}".format(row[date_column_name],row['Artwork'].replace("\n","")[0:10])
+
+        # Set up the URL ahead of time - will act as a Key for the artwork
+        artwork_dictionary['uniqueUrl'] = "{0}-{1}".format(row[date_column_name],row['Artwork'].replace("\n","").strip()[0:10])
+        artwork_dictionary['uniqueUrl'] = urlify(artwork_dictionary['uniqueUrl'] )
+        artwork_dictionary['uniqueUrl'] = urllib3.util.parse_url(artwork_dictionary['uniqueUrl']).url
+
+        #full relative URL in order to link to other pieces
+        artwork_dictionary['UniqueURLKey'] = os.path.join(base_url, artwork_dictionary['uniqueUrl'])
+
         artwork_dictionary['spoilers'] = row['Spoilers']
 
         if row['title'] == "":
-            print("Title is missing for |{0}|".format(artworkname))
+            print("    Title is missing for |{0}|".format(artworkname))
 
         if row['characters'] == "":
-            print("characters is missing for |{0}|".format(artworkname))
+            print("    characters is missing for |{0}|".format(artworkname))
 
+        # URLS -------------------------------------------------
         artwork_dictionary['urls'] = []
 
+        # Gather URL posts on other websites
         site_name = "Tumblr"
         if row['{0} URL'.format(site_name)] != "":
             artwork_dictionary[urls_key].append({'sitename': site_name, 'url': row['{0} URL'.format(site_name)]})
@@ -119,44 +236,57 @@ def make_url_dict(mydataframe: pandas.DataFrame) -> list:
         if row['{0} URL'.format(site_name)] != "" and row['{0} URL'.format(site_name)].upper() != "Drafted".upper():
             artwork_dictionary[urls_key].append({'sitename': site_name.lower(), 'url': row['{0} URL'.format(site_name)]})
 
+        # IMAGES -------------------------------------------------
         artwork_dictionary['imgs'] = make_img_list(row)
 
         # Add Image thumbnail and alt text if present
+        artwork_dictionary['thumbnailUrl'] = ""
+        artwork_dictionary['thumbnailAlt'] = ""
         if row['IMG THMB'] != "" or row['ALT THMB'] != "":
             if row['IMG THMB'] == "":
-                print("IMG THMB is missing for |{0}|".format(artworkname))
+                print("    IMG THMB is missing for |{0}|".format(artworkname))
             elif row['ALT THMB'] == "":
-                print("ALT THMB is missing for |{0}|".format(artworkname))
+                print("    ALT THMB is missing for |{0}|".format(artworkname))
             else:
                 artwork_dictionary['thumbnailUrl'] = row['IMG THMB']
                 artwork_dictionary['thumbnailAlt'] = row['ALT THMB']
 
-        artwork_dictionary['fandom'] = row['fandom'].split(",")
-
+        # CHECK IF HAVE MINIMUM -----------------------------------
+        # Add if img + alt was found, otherwise, print warning message
         if artwork_dictionary['imgs']:
             # completed, add to final dictionary
             tmp_dict[dict_key].append(artwork_dictionary)
         else:
             print("    Skipped over adding entry. No Img urls found for |{0}|\n".format(row['Artwork'].replace("\n","")[0:40]))
 
+        # VIDEOS -------------------------------------------------
+        artwork_dictionary['vids'] = make_vid_list(row)
+
+         # FANDOM LISTING -----------------------------------------
+        artwork_dictionary['fandom'] = row['fandom'].split(",")
+
+        # List of related artworks
+        # Add Image thumbnail and alt text if present
+        if row['RelatedSeries'] != "" or row['RelatedSeriesOrder'] != "":
+            update_related_dictionary(row, artwork_dictionary)
+            
+            # Add to artwork so it can link back! Complex List of Lists
+            artwork_dictionary['RelatedSeriesAndURL'] = [[u.strip(), urlify(u)] for u in row['RelatedSeries'].split(";")]
+
     return_list.append(tmp_dict)
     return return_list
 
-def method1(nsOnly: pandas.DataFrame) -> str:
-    artwork_dictionary_list = make_url_dict(nsOnly)
+def method1(nsOnly: pandas.DataFrame,base_url) -> str:
+    artwork_dictionary_list = make_url_dict(nsOnly,base_url)
     
     # remove the outer dictionary/list
     nested_json = json.dumps(artwork_dictionary_list[0]["ARTWORK"], indent=2)
 
-    #TO DO?
-    # Another variant of the dumps() function, called dump(), simply serializes the object to a text file. So if f is a text file object opened for writing, we can do this:
-    # json.dump(x, f)
-
     return nested_json
 
-def main(sheet_name, file_path_output, fandoms = [""], include_spoilers = True):
+def main(sheet_name, file_path_output, fandoms = [""], include_spoilers = True, base_url="/"):
     print('Processing: {0}. Include Spoilers: {1}'.format(sheet_name,  include_spoilers))
-    print('  1. Reading Excel File for {0}'.format(sheet_name))
+    print(' 1. Reading Excel File for {0}'.format(sheet_name), end="...\n")
 
     # Read Excel file
     excel_data_df = pandas.read_excel(
@@ -166,9 +296,11 @@ def main(sheet_name, file_path_output, fandoms = [""], include_spoilers = True):
                  'Spoilers',
                 'characters','fandom','PF tags',
                 'title','summary','detail',
+                'RelatedSeries','RelatedSeriesOrder',
                 'IMG THMB', 'ALT THMB',
                 'IMG 1','IMG 2','IMG 3','IMG 4','IMG 5','IMG 6','IMG 7','IMG 8',
                 'ALT 1','ALT 2','ALT 3','ALT 4','ALT 5','ALT 6','ALT 7','ALT 8',
+                'VID 1',
                 'Tumblr URL',
                 'Pillowfort URL',
                 'Bluesky URL',
@@ -205,38 +337,49 @@ def main(sheet_name, file_path_output, fandoms = [""], include_spoilers = True):
     # sort
     nightshadeOnly.sort_values(by=[date_column_name], inplace=True, ascending=False)
 
-    print("   2. Processing DataFrame.")
+    print(" 2. Processing DataFrame", end="...\n")
     # Convert DataFrame to JSON
-    json_str = method1(nightshadeOnly)
+    json_str = method1(nightshadeOnly, base_url)
 
-    print("    3. Updating JSON Data.")
+    print(" 3. Updating JSON Data", end="...\n")
     # Output - can ge to the json file in the src area
     with open(file_path_output, 'w', encoding='utf-8') as f:
         f.write(json_str)
 
-    print("     4. Completed.\n")
+    print(" 4. Completed!\n")
 
 if __name__ == '__main__':
     # TWEWY OG
     file_path_output = os.path.join(file_path_output_path, filename_output_twewy_og_all)
-    main('TWEWY Series', file_path_output, fandoms = ["TWEWY"], include_spoilers=True)
+    main('TWEWY Series', file_path_output, fandoms = ["TWEWY"], include_spoilers=True, base_url="art/twewy/")
 
     file_path_output = os.path.join(file_path_output_path, filename_output_twewy_og_nospoilers)
-    main('TWEWY Series', file_path_output, fandoms = ["TWEWY"], include_spoilers=False)
+    main('TWEWY Series', file_path_output, fandoms = ["TWEWY"], include_spoilers=False, base_url="art/twewy/")
 
     # NEO TWEWY
     file_path_output = os.path.join(file_path_output_path, filename_output_twewy_neo_nospoilers)
-    main('TWEWY Series', file_path_output, fandoms = ["NTWEWY"], include_spoilers=False)
+    main('TWEWY Series', file_path_output, fandoms = ["NTWEWY"], include_spoilers=False, base_url="art/twewy/")
 
     file_path_output = os.path.join(file_path_output_path, filename_output_twewy_neo_all)
-    main('TWEWY Series', file_path_output, fandoms = ["NTWEWY"], include_spoilers=True)
+    main('TWEWY Series', file_path_output, fandoms = ["NTWEWY"], include_spoilers=True, base_url="art/twewy/")
 
     # TWEWY Series
     file_path_output = os.path.join(file_path_output_path, filename_output_twewy_series_nospoilers)
-    main('TWEWY Series', file_path_output, fandoms = ["TWEWY, NTWEWY"], include_spoilers=False)
+    main('TWEWY Series', file_path_output, fandoms = ["TWEWY, NTWEWY"], include_spoilers=False, base_url="art/twewy/")
 
     file_path_output = os.path.join(file_path_output_path, filename_output_twewy_series_all)
-    main('TWEWY Series', file_path_output, fandoms = ["TWEWY, NTWEWY"], include_spoilers=True)
+    main('TWEWY Series', file_path_output, fandoms = ["TWEWY, NTWEWY"], include_spoilers=True, base_url="art/twewy/")
+
+    # Write Related Series JSON
+    # Output - can ge to the json file in the src area
+
+    # for rel_list_item in related_series_list:
+    #     rel_list_item["SeriesEntries"].sort(key=lambda x: x["Index"])
+
+    # nested_json = json.dumps(related_series_list, indent=2)
+    # file_path_output = os.path.join(file_path_output_path, filename_output_related_twewy)
+    # with open(file_path_output, 'w', encoding='utf-8') as f:
+    #     f.write(nested_json)
 
     # Persona 5 Sheet
     # file_path_output = os.path.join(file_path_output_path, filename_output_p5)
