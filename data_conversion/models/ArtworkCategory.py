d264358e7a31bb627c1157973e8d17a6f9cd59e1
gramophoneturtle <163605491+gramophoneturtle@users.noreply.github.com>
2025-03-14 14:49:15 -0600

    25 02 01 (#74)
    Site:
    * CROSSOVER ART SUPPORT
    - Art Fandom description is a little smaller
    - OC Pages updated slightly.
    
    * Links added
    - Asura's Wrath - Punch The Sky
    - I've Got My Eyes On You (Pifo, BSky)
    
    Image URLs updated + new thumbnail;
    - Kubo, Higher Vibe Frequency
    
    Artwork:
    - Two Totez Besties Drawin
    - Cheer Up, Sota!
    - Coat Tassels
    - I've got my Eyes on you
    - BDay Hanekoma
    - Thank You Bouquet
    - Mother Pikmin
    - Would You Take My Hand?
    - Snacks! Nom Nom!
    - Hazuki Inkagi
    - Didn't You Know? The Dead Don't Age
    
    Related Series (temp):
    - Commission
    
    Art Misc:
    * Added Nao and Sota to artwork character sorting
    * Updated order of A Healthy Dose of Chaos related series
    
    * Start of Goroawase Analysis
    - TWEWY text section written up
    - Animation sections written up and screenshots
    - Goroawase - refs and neo section worked on
    
    * RSS Feeds Updated

--- data_conversion/models/ArtworkCategory.py
+++ data_conversion/models/ArtworkCategory.py
@@ -3,10 +3,11 @@ class ArtworkCategory:
     spoiler_free_filename = "-spoiler-free"
     filename_ext = ".json"
 
-    def __init__(self, sheet_name = "", output_path = "", fandoms_list = [], base_url = ""):
+    def __init__(self, sheet_name = "", output_path = "", fandoms_list = [], base_url = "", new_base_url = ""):
         self.SheetName = sheet_name
         self.Fandoms = fandoms_list
         self.BaseURL = base_url
+        self.NewBaseURL = new_base_url
 
         self.OutputPath = output_path
 
